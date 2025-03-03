cfda6e922a088330c2567e9077b049bd3a7eecc8
Zayed <zayedkh02@gmail.com>
2025-03-03 11:36:47 +0000

    Added switch statements for better readability rather than else if branches

--- main.cpp
+++ main.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
 
 int main() {
-
     double earthWeight = 0.0;
     int planetNumber = 0;
 
     std::cout << "What is your weight on earth?\n";
     std::cin >> earthWeight;
 
-    std::cout << "\nHere is the planet information!\n1: Mercury\n2: Venus\n3: Mars\n4: Jupiter\n5: Saturn\n6: Uranus\n7: Neptune\nPlease enter a number for the planet you'd like to fight on!";
+    std::cout <<
+            "\nHere is the planet information!\n1: Mercury\n2: Venus\n3: Mars\n4: Jupiter\n5: Saturn\n6: Uranus\n7: Neptune\nPlease enter a number for the planet you'd like to fight on!\n";
     std::cin >> planetNumber;
 
-    if (planetNumber == 1) {
-        earthWeight = earthWeight * 0.38;
-    } else if (planetNumber == 2) {
-        earthWeight = earthWeight * 0.91;
-    } else if (planetNumber == 3) {
-        earthWeight = earthWeight * 0.38;
-    } else if (planetNumber == 4) {
-        earthWeight = earthWeight * 2.34;
-    } else if (planetNumber == 5) {
-        earthWeight = earthWeight * 1.06;
-    } else if (planetNumber == 6) {
-        earthWeight = earthWeight * 0.92;
-    } else if (planetNumber == 7) {
-        earthWeight = earthWeight * 1.19;
-    } else {
-        std::cout << "Invalid planet number entered.\n";
-        return 1;  // Exit if invalid planet number
+    switch (planetNumber) {
+        case 1:
+            earthWeight = earthWeight * 0.38;
+            break;
+        case 2:
+            earthWeight = earthWeight * 0.91;
+            break;
+        case 3:
+            earthWeight = earthWeight * 0.38;
+            break;
+        case 4:
+            earthWeight = earthWeight * 2.34;
+            break;
+        case 5:
+            earthWeight = earthWeight * 1.06;
+            break;
+        case 6:
+            earthWeight = earthWeight * 0.92;
+            break;
+        case 7:
+            earthWeight = earthWeight * 1.19;
+            break;
+        default:
+            std::cout << "Invalid planet number entered.\n";
+            return 1; // Exit if invalid planet number
     }
-    std::cout << "Your weight on the selected planet is: " << earthWeight << " units.\n";
 
+    std::cout << "Your weight on the selected planet is: " << earthWeight << " units.\n";
 }
